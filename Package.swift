93cbf0ebfd776954d2c1be7ef901c317626b43cd
Gage <gage@draftroomstudios.com>
2025-12-29 21:44:14 -0500

    Add products declaration for Mint support

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "sprout",
     platforms: [.macOS(.v15)],
+    products: [
+        .executable(name: "sprout", targets: ["sprout"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
         .package(url: "https://github.com/dduan/TOMLDecoder.git", from: "0.4.0"),
